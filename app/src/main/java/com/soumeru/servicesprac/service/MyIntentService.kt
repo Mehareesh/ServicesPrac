17d08be2f52d330eaaa910251fb4cd3b44c78a7e
Mehareesh Meruga <mehareeshm@gmail.com>
2022-07-18 20:11:00 +0530

    Initial commit for Intent Service in Android

---
+++ app/src/main/java/com/soumeru/servicesprac/service/MyIntentService.kt
@@ -0,0 +1,35 @@
+package com.soumeru.servicesprac.service
+
+import android.app.IntentService
+import android.content.Intent
+import android.util.Log
+
+class MyIntentService : IntentService("MyService") {
+
+    init {
+        instance = this
+    }
+
+    companion object {
+        lateinit var instance: MyIntentService private set
+        var isRunning = false
+
+        fun stopService() {
+            Log.d(MyIntentService::class.java.name, "Service is stopped...!")
+            isRunning = false
+            instance.stopSelf()
+        }
+    }
+
+    override fun onHandleIntent(intent: Intent?) {
+        try {
+            isRunning = true
+            while (isRunning) {
+                Log.e(MyIntentService::class.java.name, "Service is running on " + Thread.currentThread())
+                Thread.sleep(1000)
+            }
+        } catch (e: InterruptedException) {
+            Thread.currentThread().interrupt()
+        }
+    }
+}
